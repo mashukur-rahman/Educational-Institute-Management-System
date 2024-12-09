3d3d0b71d3140b18c9f01b38fa24f87599fde6cc
Zayadul-huq-afnan <zhuqafnan@gmail.com>
2024-12-09 14:39:33 +0600

    enroll_list added

---
+++ Backend/Admin_Dashboard/enroll_list.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const app = express();
+const mysql = require("mysql2");
+
+const connection=mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'eims'
+})
+
+const enroll_list = app.get("/", (req, res) => {
+    const course_code = req.body.course_code;
+    const query = `
+                    SELECT
+                        student.student_id,
+                        student.name,
+                        student.email,
+                        enrolled_list.course_code,
+                        enrolled_list.semester
+                    FROM 
+                        student
+                    INNER JOIN 
+                        enrolled_list
+                    ON 
+                        student.student_id = enrolled_list.student_id
+                    WHERE 
+                        enrolled_list.course_code = ?
+                    `;
+    
+    connection.query(query, [course_code], (err, results) => {
+        if (err) throw err;
+        //console.log(results);
+        res.json(results);
+    });               
+});
+
+module.exports = app;
--- Backend/index.js
+++ Backend/index.js
@@ -4,17 +4,22 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const mysql = require("mysql2");
 const login = require("./Auth/login");
-// const connection=mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : '',
-//     database : 'eims'
-// })
+const enroll_list = require("./Admin_Dashboard/enroll_list");
+const connection=mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'eims'
+})
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+//routes
 app.use("/api/login", login);
+app.use("/api/enroll_list", enroll_list);
+
+
 
 app.listen(3000, (req, res) => {
   console.log("Server listening at port 3000");
