47051ba34c6a598c5c9d7297b1885a588b460bf2
Zayadul-huq-afnan <zhuqafnan@gmail.com>
2024-12-10 13:26:53 +0600

    added course resources, also a test file

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,9 @@ const create_course = require("./Admin_Dashboard/create_course");
 const registration = require("./Auth/registration");
 const assign_faculty = require("./Admin_Dashboard/assign_faculty");
 const delete_course = require("./Admin_Dashboard/delete_course");
-const edit_course = require("./Course_Management/edit_course");
+const edit_course = require("./Course_Management/edit_courses");
+const course_resources = require("./Course_Management/course_resources");
+
 //aaaaa
 //comment
 const addUser = require("./Auth/addUser");
@@ -32,6 +34,8 @@ app.use("/api/create_course",create_course);
 app.use("/api/assign_faculty",assign_faculty);
 app.use("/api/delete_course",delete_course);
 app.use("/api/edit_course",edit_course);
+app.use("/api/course_resources", course_resources);
+
 
 app.listen(3000, (req, res) => {
   console.log("Server listening at port 3000");
