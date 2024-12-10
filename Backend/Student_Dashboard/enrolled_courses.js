41cc1d0427ae76f187ef1cd5eefd7eddafd4a2fd
Zayadul-huq-afnan <zhuqafnan@gmail.com>
2024-12-10 17:34:04 +0600

    update enrolled courses

--- Backend/Student_Dashboard/enrolled_courses.js
+++ Backend/Student_Dashboard/enrolled_courses.js
@@ -12,15 +12,29 @@ const connection = mysql.createConnection({
 });
 
 const enrolled_courses = app.post("/", (req, res) => {
-
-    const email = req.body.email;
+  const email = req.body.email;
     
-    console.log(req.body);
+    //console.log(req.body);
     const query = `
-                    SELECT course_code, semester, section
-                    FROM enrolled_list
-                    WHERE
-                         email = ?
+                    SELECT
+                        course_details.course_code,
+                        course_details.section,
+                        course_details.course_name,
+                        course_details.theory_room,
+                        course_details.theory_timing,
+                        course_details.lab_room,
+                        course_details.lab_timing,
+                        course_details.faculty_email,
+                        course_details.faculty_initial,
+                        course_details.semester
+                    FROM 
+                        course_details
+                    INNER JOIN 
+                        enrolled_list
+                    ON 
+                        course_details.course_code = enrolled_list.course_code
+                    WHERE 
+                        enrolled_list.email = ?
                     `;
 
   connection.query(query, [email], (err, result) => {
